403af0c7fd30983c69b442ddde87595541c77273
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-03 18:17:48 +0200

    Added calculator Object which have add(), subtract(), divide() and multiply() functions. Added a add() test for the calculator Object

--- index.js
+++ index.js
@@ -19,8 +19,19 @@ function reverseString(phrase) {
 }
 
 let calculator = {
-
-}
+	add: (num1, num2) => {
+		return num1 + num2;
+	},
+	subtract: (num1, num2) => {
+		return num1 - num2;
+	},
+	divide: (num1, num2) => {
+		return num1 / num2;
+	},
+	multiply: (num1, num2) => {
+		return num1 * num2;
+	},
+};
 
 function caesarCipher() {}
 
--- index.test.js
+++ index.test.js
@@ -1,5 +1,7 @@
 import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./index";
 
+// Capitalize
+
 test("capitalize(): europa", () => {
 	expect(capitalize("europa")).toBe("Europa");
 });
@@ -12,10 +14,18 @@ test("capitalize(): 007", () => {
 	expect(capitalize("007")).toBe("007");
 });
 
+// ReverseString
+
 test("reverseString(): alfonso sa Mangiare", () => {
 	expect(reverseString("alfonso sa Mangiare")).toBe("eraignaM as osnofla");
 });
 
 test("reverseString(): eraignaM as osnofla", () => {
 	expect(reverseString("eraignaM as osnofla")).toBe("alfonso sa Mangiare");
+});
+
+// Calculator
+
+test("1 add 2 returns 3", () => {
+	expect(calculator.add(1, 2)).toBe(3);
 });
