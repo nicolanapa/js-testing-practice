d4624e64a177907957b2e8b2694f9ee1cf41e93e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-05-03 17:35:14 +0200

    Wrote code for the capitalize() function which makes the first char capitalized

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function capitalize(phrase) {}
+function capitalize(phrase) {
+	if (phrase[0].charCodeAt() >= 97 && phrase[0].charCodeAt() <= 122) {
+        let temp = phrase.slice(0, 1);
+		temp = String.fromCharCode(phrase[0].charCodeAt() - 32);
+        phrase = temp + phrase.slice(1);
+	}
+
+	return phrase;
+}
 
 export { capitalize };
